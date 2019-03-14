bb4a0e4795eb8388c7138ad824e982c043b2f4f1
tonyp7 <bubbathemaster@gmail.com>
2019-03-14 23:09:34 +0800

    MIT header + code comment

--- SPINixieDriver.h
+++ SPINixieDriver.h
@@ -1,3 +1,33 @@
+/*
+Copyright (c) 2019 Tony Pottier
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+@file SPINixieDriver.h
+@author Tony Pottier
+@brief Arduino library to easily communicated with daisy-chained Nixie tubes.
+
+@see https://idyl.io
+@see https://github.com/tonyp7/SPINixieDriver
+*/
+
+
 #ifndef SPINixieDriver_h
 #define SPINixieDriver_h
 
@@ -7,13 +37,35 @@ class SPINixieDriver {
   public:
 	SPINixieDriver();
 	~SPINixieDriver();
-	void begin( int rckPin, int clkPin, int dataPin, int outputEnablePin, bool useHardwareSPI );
+	
+	/** @brief Initialize the driver
+	*	this should be called in the "setup" part of an Arduino program or outside a loop otherwise.
+	*	If hardwareSPI is set to true, the pins specified must be the correct SPI pins of the board being used.
+	*	Output Enable is optional and can be set to 0 if not used.
+	*	@param rckPin The pin used as a "chip select". Technically the latch register clock.
+	*	@param clkPin The pin used for serial clock.
+	*	@param dataPin The pin used to transmit data. In the case of hardware SPI this is the MOSI pin.
+	*	@param useHardwareSPI If set to true, Arduino SPI library will be used. Otherwise, software serial will be used via the shiftOut function.
+	*/
+	void begin( int rckPin, int clkPin, int dataPin, int outputEnablePin = 0, bool useHardwareSPI = true);
+	
+	
+	/** @brief Enable/disable output 
+	*	Please note that as Output Enable pin uses reverse logic, setting "true" will actually pull the output enable pin LOW.
+	*/
 	void outputEnable( bool value );
+	
+	/** @brief kills the driver. If software serial is used this servers no purpose */
 	void end();
 	
-	/** @brief Sends an array of data over daisy-chained IN-12
+	/** @brief Sends an array of data over daisy-chained nixie tube
+	*	@param data an array containing numbers to display
 	*/
 	void send(const uint8_t *data);
+	
+	/** @brief Sends a single digit over to a nixie tube
+	*	@param data the digit value to display
+	*/
 	void send(const uint8_t data);
 
   private:
